9f59561b1f09752eac7592a70074eedf06260db2
Silviu Hermeneanu <silh6767@gmail.com>
2025-06-18 01:18:34 +0300

    WIP: Save unstaged changes before rebase

--- CyberSimLab/obj/Debug/net9.0/CyberSimLab.AssemblyInfo.cs
+++ CyberSimLab/obj/Debug/net9.0/CyberSimLab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSimLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e0b97b073cc9c1c79fe9198de9f0e6bc2e26ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSimLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSimLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
